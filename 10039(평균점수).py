874b345115a681cd1e12dc8eaab2d8d680813e78
Park <deu04088@naver.com>
2023-04-13 10:03:24 +0900

    4월 13일

---
+++ 10039(평균점수).py
@@ -0,0 +1,18 @@
+# won = int(input())
+# se = int(input())
+# sang = int(input())
+# sueng = int(input())
+# kang = int(input())
+
+# list = [won, se, sang, sueng, kang]
+# sum = 0
+
+# for i in list:
+#     if i < 40:
+#         i = 40
+#     else:
+li = []
+for i in range(5):        
+    li.append(map(int, input()))
+
+print(li)
